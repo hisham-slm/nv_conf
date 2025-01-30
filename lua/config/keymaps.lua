5c26675a5df488a733ee3ecd676df0c57cd14402
hisham-slm <hishamsalam350@gmail.com>
2025-01-30 12:26:34 +0530

    add del markers
    added dm{letter} keybinding to remove the marker at the specific letter
    (execute delm:{letter} command)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,6 @@
 vim.api.nvim_set_keymap("i", "kj", "<Esc>", { noremap = true, silent = true })
-
+-- Create a mapping for 'dm' followed by a letter to delete marks
+vim.keymap.set("n", "dm", function()
+  local mark = vim.fn.nr2char(vim.fn.getchar())
+  vim.cmd("delmarks " .. mark)
+end, { noremap = true })

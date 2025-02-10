92f9a591fa5cc3439c14f87fc67af80cdd092adf
hisham-slm <hishamsalam350@gmail.com>
2025-02-10 22:33:01 +0530

    disabled mouse and arrow keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,14 @@ vim.keymap.set("n", "dm", function()
   local mark = vim.fn.nr2char(vim.fn.getchar())
   vim.cmd("delmarks " .. mark)
 end, { noremap = true })
+
+-- disabling arrow keys
+vim.api.nvim_set_keymap("n", "<Up>", "<NOP>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Down>", "<NOP>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Left>", "<NOP>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Right>", "<NOP>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("i", "<Up>", "<NOP>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Down>", "<NOP>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Left>", "<NOP>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Right>", "<NOP>", { noremap = true, silent = true })
